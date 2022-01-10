7c295c249889ea3154e735a87e49d14a694ef313
Byron Katz <byronka@msn.com>
2022-01-09 19:48:27 -0500

    start TDD on HTTP parsing

--- test/primary/Tests.java
+++ test/primary/Tests.java
@@ -6,6 +6,8 @@ import utils.ExtendedExecutor;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 class Tests {
 
@@ -13,135 +15,153 @@ class Tests {
 
     ExecutorService es = ExtendedExecutor.makeExecutorService();
     Logger logger = new Logger(es).initialize(); //.turnOff(Logger.Type.DEBUG);
-    Web web = new Web(logger);
 
-    logger.test("a happy path");
-    {
-      int result = Main.add(2, 3);
-      assertEquals(result, 5);
-    }
+    try {
 
-    logger.test("a couple negatives");
-    {
-      int result = Main.add(-2, -3);
-      assertEquals(result, -5);
-    }
+      Web web = new Web(logger);
 
-    logger.test("with zeros");
-    {
-      int result = Main.add(0, 0);
-      assertEquals(result, 0);
-    }
+      logger.test("a happy path");
+      {
+        int result = Main.add(2, 3);
+        assertEquals(result, 5);
+      }
 
-    logger.test("client / server");
-    {
-      try (Web.Server primaryServer = web.startServer(es)) {
-        try (Web.SocketWrapper client = web.startClient(primaryServer)) {
-          try (Web.SocketWrapper server = primaryServer.getServer(client)) {
+      logger.test("a couple negatives");
+      {
+        int result = Main.add(-2, -3);
+        assertEquals(result, -5);
+      }
 
-            client.send("hello foo!\n");
-            String result = server.readLine();
-            assertEquals("hello foo!", result);
+      logger.test("with zeros");
+      {
+        int result = Main.add(0, 0);
+        assertEquals(result, 0);
+      }
+
+      logger.test("client / server");
+      {
+        try (Web.Server primaryServer = web.startServer(es)) {
+          try (Web.SocketWrapper client = web.startClient(primaryServer)) {
+            try (Web.SocketWrapper server = primaryServer.getServer(client)) {
+
+              client.send("hello foo!\n");
+              String result = server.readLine();
+              assertEquals("hello foo!", result);
+            }
           }
         }
       }
-    }
 
-    logger.test("client / server with more conversation");
-    {
-      String msg1 = "hello foo!";
-      String msg2 = "and how are you?";
-      String msg3 = "oh, fine";
+      logger.test("client / server with more conversation");
+      {
+        String msg1 = "hello foo!";
+        String msg2 = "and how are you?";
+        String msg3 = "oh, fine";
 
-      try (Web.Server primaryServer = web.startServer(es)) {
-        try (Web.SocketWrapper client = web.startClient(primaryServer)) {
-          try (Web.SocketWrapper server = primaryServer.getServer(client)) {
+        try (Web.Server primaryServer = web.startServer(es)) {
+          try (Web.SocketWrapper client = web.startClient(primaryServer)) {
+            try (Web.SocketWrapper server = primaryServer.getServer(client)) {
 
-            // client sends, server receives
-            client.send(withNewline(msg1));
-            assertEquals(msg1, server.readLine());
+              // client sends, server receives
+              client.send(withNewline(msg1));
+              assertEquals(msg1, server.readLine());
 
-            // server sends, client receives
-            server.send(withNewline(msg2));
-            assertEquals(msg2, client.readLine());
+              // server sends, client receives
+              server.send(withNewline(msg2));
+              assertEquals(msg2, client.readLine());
 
-            // client sends, server receives
-            client.send(withNewline(msg3));
-            assertEquals(msg3, server.readLine());
+              // client sends, server receives
+              client.send(withNewline(msg3));
+              assertEquals(msg3, server.readLine());
+            }
           }
         }
       }
-    }
 
-    // no need to run this every time.  Feel free to uncomment this and make
-    // sure it works, but seeing exceptions in the output from tests is disconcerting.
-    // logger.test("What happens if we throw an exception in a thread");
-    {
-      // es.submit(() -> {throw new RuntimeException("No worries folks, just testing the exception handling");});
-    }
+      // no need to run this every time.  Feel free to uncomment this and make
+      // sure it works, but seeing exceptions in the output from tests is disconcerting.
+      // logger.test("What happens if we throw an exception in a thread");
+      {
+        // es.submit(() -> {throw new RuntimeException("No worries folks, just testing the exception handling");});
+      }
 
-    logger.test("like we're a web server");
-    {
-      try (Web.Server primaryServer = web.startServer(es)) {
-        try (Web.SocketWrapper client = web.startClient(primaryServer)) {
-          try (Web.SocketWrapper server = primaryServer.getServer(client)) {
+      logger.test("like we're a web server");
+      {
+        try (Web.Server primaryServer = web.startServer(es)) {
+          try (Web.SocketWrapper client = web.startClient(primaryServer)) {
+            try (Web.SocketWrapper server = primaryServer.getServer(client)) {
+              // send a GET request
+              client.sendHttpLine("GET /index.html HTTP/1.1");
+              client.sendHttpLine("cookie: abc=123");
+              client.sendHttpLine("");
+              server.sendHttpLine("HTTP/1.1 200 OK");
+            }
+          }
+        }
+      }
+
+      /**
+       * If we provide some code to handle things on the server
+       * side when it accepts a connection, then it will more
+       * truly act like the web server we want it to be.
+       */
+      logger.test("starting server with a handler");
+      {
+        /**
+         * Simplistic proof-of-concept of the primary server
+         * handler.  The socket has been created and as new
+         * clients call it, this method handles each request.
+         *
+         * There's nothing to prevent us using this as the entire
+         * basis of a web framework.
+         */
+        Consumer<Web.SocketWrapper> handler = (sw) -> logger.logDebug(sw::readLine);
+
+        try (Web.Server primaryServer = web.startServer(es, handler)) {
+          try (Web.SocketWrapper client = web.startClient(primaryServer)) {
             // send a GET request
             client.sendHttpLine("GET /index.html HTTP/1.1");
-            client.sendHttpLine("cookie: abc=123");
-            client.sendHttpLine("");
-            server.sendHttpLine("HTTP/1.1 200 OK");
+
+            // give the server time to run code from the handler,
+            // then shut down.
+            primaryServer.centralLoopFuture.get(10, TimeUnit.MILLISECONDS);
+          } catch (Exception ex) {
+            // do nothing
           }
         }
       }
-    }
 
-    /**
-     * If we provide some code to handle things on the server
-     * side when it accepts a connection, then it will more
-     * truly act like the web server we want it to be.
-     */
-    logger.test("starting server with a handler");
-    {
-      /**
-       * Simplistic proof-of-concept of the primary server
-       * handler.  The socket has been created and as new
-       * clients call it, this method handles each request.
-       *
-       * There's nothing to prevent us using this as the entire
-       * basis of a web framework.
-       */
-      Consumer<Web.SocketWrapper> handler = (sw) -> logger.logDebug(sw::readLine);
+      // Just a simple test while controlling the fake socket wrapper
+      logger.test("TDD of a handler");
+      {
+        FakeSocketWrapper sw = new FakeSocketWrapper();
+        AtomicReference<String> result = new AtomicReference<>();
+        sw.sendHttpLineAction = s -> result.set(s);
 
-      try (Web.Server primaryServer = web.startServer(es, handler)) {
-        try (Web.SocketWrapper client = web.startClient(primaryServer)) {
-          // send a GET request
-          client.sendHttpLine("GET /index.html HTTP/1.1");
+        // this is what we're really going to test
+        Consumer<Web.ISocketWrapper> handler = (socketWrapper) -> socketWrapper.sendHttpLine("this is a test");
+        handler.accept(sw);
 
-          // give the server time to run code from the handler,
-          // then shut down.
-          primaryServer.centralLoopFuture.get(10, TimeUnit.MILLISECONDS);
-        } catch (Exception ex) {
-          // do nothing
-        }
+        assertEquals("this is a test", result.get());
       }
+
+      // See https://developer.mozilla.org/en-US/docs/Web/HTTP/Messages#start_line
+      // 1. the method (GET, POST, etc.)
+      // 2. the request target
+      // 3. the HTTP version (e.g. HTTP/1.1)
+      logger.test("We should be able to pull valuable information from the start line");
+      {
+        Pattern startLineRegex = Pattern.compile("(GET|POST) /(.*) HTTP/(?:1.1|1.0)");
+        Matcher m = startLineRegex.matcher("GET /index.html HTTP/1.1");
+        assertTrue(m.matches());
+      }
+
+
+    } finally {
+      // final shutdown pieces
+      logger.stop();
+      es.shutdownNow();
     }
-
-    logger.test("TDD of a handler");
-    {
-      FakeSocketWrapper sw = new FakeSocketWrapper();
-      AtomicReference<String> result = new AtomicReference<>();
-      sw.sendHttpLineAction = s -> result.set(s);
-
-      // this is what we're really going to test
-      Consumer<Web.ISocketWrapper> handler = (socketWrapper) -> socketWrapper.sendHttpLine("this is a test");
-      handler.accept(sw);
-
-      assertEquals("this is a test", result.get());
-    }
-
-    // final shutdown pieces
-    logger.stop();
-    es.shutdownNow();
   }
 
   /**
@@ -156,6 +176,12 @@ class Tests {
   private static void assertEquals(String left, String right) {
     if (!left.equals(right)) {
       throw new RuntimeException("Not equal! left: " + left + " right: " + right);
+    }
+  }
+
+  private static void assertTrue(boolean value) {
+    if (!value) {
+      throw new RuntimeException("value was unexpectedly false");
     }
   }
 
