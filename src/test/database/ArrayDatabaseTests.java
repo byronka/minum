f082800c8dacb4d1057bd649d4ef360aa00531c3
Byron Katz <byronka@msn.com>
2022-05-14 18:16:37 -0400

    More testing, trying it with a list instead of an array

--- src/test/database/ArrayDatabaseTests.java
+++ src/test/database/ArrayDatabaseTests.java
@@ -2,9 +2,11 @@ package database;
 
 import logging.TestLogger;
 
+import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 
 import static framework.TestFramework.assertEquals;
+import static framework.TestFramework.assertTrue;
 
 public class ArrayDatabaseTests {
     private final TestLogger logger;
@@ -23,6 +25,8 @@ public class ArrayDatabaseTests {
          *
          * Here, we'll play a bit. CRUD - create, read, update, delete.
          * Add some data, search for some data, update the data, delete some data
+         *
+         * Trying it this time with an array
          */
         logger.test("how well does a pure array database suit us");
         {
@@ -48,6 +52,91 @@ public class ArrayDatabaseTests {
                 }
             }
             assertEquals(foundFoo, new Foo("black", "taffy"));
+
+            // updating some data - change taffy to lemon where color
+            for (var i = 0; i < database.length; i++) {
+                if ( "taffy".equals(database[i][1])) {
+                    database[i][1] = "lemon";
+                }
+            }
+
+            // confirm the change
+            for (var i = 0; i < database.length; i++) {
+                if ( "black".equals(database[i][0])) {
+                    foundFoo = new Foo(database[i][0], database[i][1]);
+                }
+            }
+            assertEquals(foundFoo, new Foo("black", "lemon"));
+
+            // delete the data at "white"
+            for (var i = 0; i < database.length; i++) {
+                if ( "white".equals(database[i][0])) {
+                    database[i][0] = null;
+                    database[i][1] = null;
+                }
+            }
+
+            // confirm the delete
+            foundFoo = null;
+            for (var i = 0; i < database.length; i++) {
+                if ( "white".equals(database[i][0])) {
+                    foundFoo = new Foo(database[i][0], database[i][1]);
+                }
+            }
+            assertTrue(foundFoo == null);
+
+        }
+
+        logger.test("using a list for the data instead of an array");
+        {
+            // the schema for the data
+            record Foo(String color, String flavor){}
+
+            // the database itself
+            var database = new ArrayList<String[]>();
+
+            // adding some new data
+            database.add(new String[]{"orange", "vanilla"});
+            database.add(new String[]{"blue", "candy corn"});
+            database.add(new String[]{"white", "chocolate"});
+            database.add(new String[]{"black", "taffy"});
+            database.add(new String[]{"orange", "oreo"});
+
+            // search for data
+            Foo foundFoo = null;
+            for (String[] row : database) {
+                if ( "black".equals(row[0])) {
+                    foundFoo = new Foo(row[0], row[1]);
+                }
+            }
+            assertEquals(foundFoo, new Foo("black", "taffy"));
+
+            // updating some data - change taffy to lemon where color
+            for (String[] row : database) {
+                if ( "taffy".equals(row[1])) {
+                    row[1] = "lemon";
+                }
+            }
+
+            // confirm the change
+            for (String[] row : database) {
+                if ( "black".equals(row[0])) {
+                    foundFoo = new Foo(row[0], row[1]);
+                }
+            }
+            assertEquals(foundFoo, new Foo("black", "lemon"));
+
+            // delete the data at "white"
+            database.removeIf(row -> "white".equals(row[0]));
+
+            // confirm the delete
+            foundFoo = null;
+            for (String[] row : database) {
+                if ( "white".equals(row[0])) {
+                    foundFoo = new Foo(row[0], row[1]);
+                }
+            }
+            assertTrue(foundFoo == null);
         }
     }
 }
