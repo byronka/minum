b711c522b298901a94da2058cb4c8e02a9bf6e88
Byron Katz <byronka@msn.com>
2022-09-30 23:29:59 -0400

    Block at a precise place
    One thing I found out when trying out the virtual threads executor is
    that the system would just fall through if I didn't have something to
    "block" it at a certain point.  This line performs that service.  It
    sits there, blocking, until it "gets" a result from the central loop,
    which, oh by the way, is wrapping an infinite loop that only exits if we
    interrupt it forcibly (like by the command in Server.stop and
    FullSystem.shutdown, etc.)

--- src/main/atqa/Main.java
+++ src/main/atqa/Main.java
@@ -4,13 +4,15 @@ import atqa.logging.Logger;
 import atqa.utils.ExtendedExecutor;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
         final var es = ExtendedExecutor.makeExecutorService();
         final var logger = new Logger(es);
-        new FullSystem(logger, es).start();
+        final var fs = new FullSystem(logger, es).start();
+        fs.server.centralLoopFuture.get();
     }
 
 }
