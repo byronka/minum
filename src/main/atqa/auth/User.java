0c89c06af9342c68cebbe563bcb2f8736caa0647
Byron Katz <byronka@msn.com>
2022-12-07 09:02:57 -0800

    New User class, local copies of a few important documents

---
+++ src/main/atqa/auth/User.java
@@ -0,0 +1,14 @@
+package atqa.auth;
+
+/**
+ * A data structure representing authentication information for a user.
+ * @param id the unique identifier for this record
+ * @param username a user-chosen unique identifier for this record (system must not allow two of the same username)
+ * @param hashedPassword the hash of a user's password
+ * @param salt some randomly-generated letters appended to the user's password.  This is
+ *             to prevent dictionary attacks if someone gets access to the database contents.
+ *             See "Salting" in docs/password_storage_cheat_sheet.txt
+ * @param currentSession If this use is currently authenticated, there will be a {@link SessionId} for them
+ */
+public record User(Long id, String username, String hashedPassword, String salt, String currentSession) {
+}
