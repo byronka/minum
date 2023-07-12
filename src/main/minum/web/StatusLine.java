73f150bfdf14ef849bbaf15cbac4e9eec749a8db
Byron Katz <byronka@msn.com>
2023-07-12 18:14:28 -0400

    Including more status codes

--- src/main/minum/web/StatusLine.java
+++ src/main/minum/web/StatusLine.java
@@ -22,20 +22,54 @@ public record StatusLine(StatusCode status, WebEngine.HttpVersion version, Strin
      * See see <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Status">Status Codes</a>
      */
     public enum StatusCode{
+
+        /* Successful responses (200 – 299) */
+
         _200_OK(200, "OK"),
         _201_CREATED(201, "CREATED"),
+        _202_ACCEPTED(202, "ACCEPTED"),
+
+        /* Redirection messages (300 – 399) */
+
+        _301_MOVED_PERMANENTLY(301, "MOVED PERMANENTLY"),
+        _302_FOUND(302, "FOUND"),
 
         /**
          * Used a lot after receiving a post response.  The pattern is to
          * receive the post, then redirect to a new page. See <a href="https://en.wikipedia.org/wiki/Post/Redirect/Get">...</a>
          */
         _303_SEE_OTHER(303, "SEE OTHER"),
+        _304_NOT_MODIFIED(304, "NOT MODIFIED"),
+        _307_TEMPORARY_REDIRECT(307, "TEMPORARY REDIRECT"),
+        _308_PERMANENT_REDIRECT(308, "PERMANENT REDIRECT"),
 
+        /* Client error responses (400 – 499) */
+
+        _400_BAD_REQUEST(400, "BAD REQUEST"),
         _401_UNAUTHORIZED(401, "UNAUTHORIZED"),
         _403_FORBIDDEN(403, "FORBIDDEN"),
-        _429_TOO_MANY_REQUESTS(429, "TOO MANY REQUESTS"),
         _404_NOT_FOUND(404, "NOT FOUND"),
+        _405_METHOD_NOT_ALLOWED(405, "METHOD NOT ALLOWED"),
+        _406_NOT_ACCEPTABLE(406, "NOT ACCEPTABLE"),
+        _408_REQUEST_TIMEOUT(408, "REQUEST TIMEOUT"),
+        _409_CONFLICT(409, "CONFLICT"),
+        _410_GONE(410, "GONE"),
+        _411_LENGTH_REQUIRED(411, "LENGTH REQUIRED"),
+        _413_PAYLOAD_TOO_LARGE(413, "PAYLOAD TOO LARGE"),
+        _414_URI_TOO_LONG(414, "URI TOO LONG"),
+        _415_UNSUPPORTED_MEDIA_TYPE(415, "UNSUPPORTED MEDIA TYPE"),
+        _426_UPGRADE_REQUIRED(426, "UPGRADE REQUIRED"),
+        _429_TOO_MANY_REQUESTS(429, "TOO MANY REQUESTS"),
+        _431_REQUEST_HEADER_FIELDS_TOO_LARGE(431, "REQUEST HEADER FIELDS TOO LARGE"),
+
+        /* Server error responses (500 – 599) */
+
         _500_INTERNAL_SERVER_ERROR(500, "INTERNAL SERVER ERROR"),
+        _501_NOT_IMPLEMENTED(501, "NOT IMPLEMENTED"),
+        _502_BAD_GATEWAY(502, "BAD GATEWAY"),
+        _503_SERVICE_UNAVAILABLE(503, "SERVICE UNAVAILABLE"),
+        _504_GATEWAY_TIMEOUT(504, "GATEWAY TIMEOUT"),
+        _505_HTTP_VERSION_NOT_SUPPORTED(505, "HTTP VERSION NOT SUPPORTED"),
 
         /**
          * The null object, meant to represent "no status code"
