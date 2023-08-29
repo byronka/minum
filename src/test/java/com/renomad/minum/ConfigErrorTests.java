9aa7ea446e1bf2512a3e282f29f0d1d879db49ff
Byron Katz <byronka@msn.com>
2023-08-29 19:33:10 -0400

    Converted ConfigErrorTests

--- src/test/java/com/renomad/minum/ConfigErrorTests.java
+++ src/test/java/com/renomad/minum/ConfigErrorTests.java
@@ -1,36 +1,31 @@
 package com.renomad.minum;
 
+import com.renomad.minum.logging.ILogger;
 import com.renomad.minum.logging.TestLogger;
+import com.renomad.minum.web.InputStreamUtils;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 
-import static com.renomad.minum.testing.TestFramework.assertEquals;
-import static com.renomad.minum.testing.TestFramework.assertTrue;
+import static com.renomad.minum.testing.TestFramework.*;
 
 public class ConfigErrorTests {
-    private final TestLogger logger;
-    private final ExecutorService es;
-    private final Context context;
 
-    public ConfigErrorTests(Context context) {
-        this.logger = (TestLogger) context.getLogger();
-        this.es = context.getExecutorService();
-        this.context = context;
-        logger.testSuite("ConfigErrorTests");
+    public ConfigErrorTests() {
     }
 
-    public void tests() throws IOException {
-
-        /*
-        It's quite important that the user has a configuration file.
-        We specify it to be named minum.config in the root directory.
-         */
-        logger.test("configuration missing"); {
-            String configErrorMessage = ConfigErrorMessage.getConfigErrorMessage();
-            assertTrue(configErrorMessage.contains("No properties file found at ./minum.config"));
-            assertTrue(configErrorMessage.contains("****   Copy after this line -v    ****"));
-            assertTrue(configErrorMessage.contains("The log levels are:"));
-        }
+    /**
+     * It's quite important that the user has a configuration file.
+     * We specify it to be named minum.config in the root directory.
+     */
+    @Test
+    public void testConfigurationMissing() {
+        String configErrorMessage = ConfigErrorMessage.getConfigErrorMessage();
+        assertTrue(configErrorMessage.contains("No properties file found at ./minum.config"));
+        assertTrue(configErrorMessage.contains("****   Copy after this line -v    ****"));
+        assertTrue(configErrorMessage.contains("The log levels are:"));
     }
+
 }
