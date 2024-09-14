d562033b47544b3c9741945910d6e0a34a3688d5
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2024-09-14 18:19:49 -0400

    add module-info
    Now we can use jlink to create small runtimes with this library

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.renomad.minum {
+
+  exports com.renomad.minum.database;
+  exports com.renomad.minum.htmlparsing;
+  exports com.renomad.minum.logging;
+  exports com.renomad.minum.queue;
+  exports com.renomad.minum.security;
+  exports com.renomad.minum.state;
+  exports com.renomad.minum.templating;
+  exports com.renomad.minum.testing;
+  exports com.renomad.minum.utils;
+  exports com.renomad.minum.web;
+
+}
