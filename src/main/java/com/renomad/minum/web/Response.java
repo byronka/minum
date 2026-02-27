730fc5a6ce66472bf0c664e25765d237be00c773
Byron Katz <byronka@msn.com>
2026-02-27 16:09:35 -0500

    documentation

--- src/main/java/com/renomad/minum/web/Response.java
+++ src/main/java/com/renomad/minum/web/Response.java
@@ -247,8 +247,22 @@ public final class Response implements IResponse {
     }
 
     /**
-     * Construct a value to be used in the header map that contains multiple values separated by CRLF.
-     * @param values the list of values
+     * A helper method to construct a value to be used in the header map
+     * that contains multiple values separated by CRLF.
+     * <p>
+     *     For instance, if it is needed to set multiple cookies (using "set-cookie") in a response, this method
+     *     would be suitable. That usage might look like this:
+     * </p>
+     * <pre>
+     *     {@code
+     *      webFramework.registerPath(POST, "authenticate",
+     *                 request -> Response.buildResponse(
+     *                         CODE_200_OK,
+     *                         Map.of("Set-Cookie", Response.constructHeaderMultiValue(List.of("a=value1", "b=value2"))),
+     *                         "You have been authenticated.  Two cookies have been set: a and b"
+     *                         ));
+     *     }
+     * </pre>
      * @return a single value constructed by the list of values separated by CRLF
      */
     public static String constructHeaderMultiValue(List<String> values) {
